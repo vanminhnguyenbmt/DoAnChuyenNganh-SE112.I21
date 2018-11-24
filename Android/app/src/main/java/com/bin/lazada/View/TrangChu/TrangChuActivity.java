716720cf95ccedb05e7a817686b5ebf55646aae8
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2018-11-25 02:17:06 +0700

    Last Update

--- Android/app/src/main/java/com/bin/lazada/View/TrangChu/TrangChuActivity.java
+++ Android/app/src/main/java/com/bin/lazada/View/TrangChu/TrangChuActivity.java
@@ -47,8 +47,8 @@ import java.util.List;
 
 public class TrangChuActivity extends AppCompatActivity implements ViewXuLyMenu, GoogleApiClient.OnConnectionFailedListener, AppBarLayout.OnOffsetChangedListener {
 
-    public static final String SERVER_NAME = "http://192.168.137.243/weblazada/loaisanpham.php";
-    public static final String SERVER = "http://192.168.137.243/weblazada";
+    public static final String SERVER_NAME = "http://192.168.1.134:4433/weblazada/loaisanpham.php";
+    public static final String SERVER = "http://192.168.1.134:4433/weblazada";
 
     Toolbar toolbar;
     TabLayout tabLayout;
